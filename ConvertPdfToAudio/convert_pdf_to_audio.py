ea1b8290e5f28e81f460a59e6d821305a01b7222
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-07-25 18:33:32 -0600

    "Initial commit"

---
+++ ConvertPdfToAudio/convert_pdf_to_audio.py
@@ -0,0 +1,20 @@
+import pyttsx3
+import PyPDF2
+import pdfplumber
+
+# path of the pdf file to read
+file = "C:\Users\PC 01\Desktop\Portfolio\AutomationPythonScripts\ConvertPdfToAudio\sample.pdf"
+f = open(file, "rb")
+pdfR = PyPDF2.PdfFileReader(f)
+pages = pdfR.numPages # Get the number of pages
+
+with pdfplumber.open(file) as pdf:
+    for i in range(0, pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        print(text)
+        
+        s = pyttsx3.init()
+        s.save_to_file(text, "C:\Users\PC 01\Desktop\Portfolio\AutomationPythonScripts\ConvertPdfToAudio\audio.mp3")
+        s.runAndWait()
+f.close()
