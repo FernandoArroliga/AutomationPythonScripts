3e2a681f671e7a0ccf70d239cdca49d67b5df774
Fernando Arroliga <FernandoArroliga@hotmail.com>
2023-07-25 18:43:58 -0600

    "create the main script"

--- ConvertPdfToAudio/convert_pdf_to_audio.py
+++ ConvertPdfToAudio/convert_pdf_to_audio.py
@@ -5,8 +5,8 @@ import pdfplumber
 # path of the pdf file to read
 file = "C:/Users/PC 01/Desktop/Portfolio/AutomationPythonScripts/ConvertPdfToAudio/sample.pdf"
 f = open(file, "rb")
-pdfR = PyPDF2.PdfFileReader(f)
-pages = pdfR.numPages # Get the number of pages
+pdfR = PyPDF2.PdfReader(f)
+pages = len(pdfR.pages) # Get the number of pages
 
 with pdfplumber.open(file) as pdf:
     for i in range(0, pages):
